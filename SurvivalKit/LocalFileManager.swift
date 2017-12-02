e6fc106d2b1462be6534e83c93d64c4ff1c7d630
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2017-12-02 15:11:55 -0200

    added local file manager

---
+++ SurvivalKit/LocalFileManager.swift
@@ -0,0 +1,42 @@
+//
+//  LocalFileManager.swift
+//  SurvivalKit
+//
+//  Created by Cristiano Salla Lunardi on 12/2/17.
+//  Copyright © 2017 Alisson. All rights reserved.
+//
+
+import UIKit
+
+struct LocalFileManager {
+	private let userDefaults = UserDefaults.standard
+	
+	private let key = "Object"
+	
+	private func objectsToNSData(objects: [Any]) -> [Data]{
+		var objectsDataArray = [Data]()
+		for object in objects{
+			objectsDataArray.append(NSKeyedArchiver.archivedData(withRootObject: object))
+		}
+		return objectsDataArray
+	}
+	
+	func persistObjects(objects : [Any]){
+		let objectsData = objectsToNSData(objects: objects)
+		self.userDefaults.setValue(objectsData, forKey: key)
+		self.userDefaults.synchronize()
+	}
+	
+	func loadObjects() -> [Any]?{
+		if let object = userDefaults.value(forKey: key){
+			let objectsData = object as! [Data]
+			var objectsArray = [Any]()
+			for objectData in objectsData{
+				objectsArray.append(NSKeyedUnarchiver.unarchiveObject(with: objectData) as Any)
+			}
+			return objectsArray
+		}
+		return nil
+	}
+	
+}
