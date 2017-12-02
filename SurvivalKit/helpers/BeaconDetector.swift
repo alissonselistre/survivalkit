01270ccadeca3ad042432713aab329527351150c
Alisson Selistre <alissonselistre@gmail.com>
2017-12-02 15:19:17 -0200

    Possibility to list the detected beacons.

---
+++ SurvivalKit/helpers/BeaconDetector.swift
@@ -0,0 +1,13 @@
+//
+//  BeaconDetector.swift
+//  SurvivalKit
+//
+//  Created by Alisson L. Selistre on 02/12/17.
+//  Copyright © 2017 Alisson. All rights reserved.
+//
+
+import UIKit
+
+class BeaconDetector: NSObject {
+
+}
