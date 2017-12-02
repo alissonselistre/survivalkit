2c072690467b6be892c40b48d923cad65642b690
Alisson Selistre <alissonselistre@gmail.com>
2017-12-02 12:09:45 -0200

    Initial Commit

---
+++ SurvivalKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SurvivalKit
+//
+//  Created by Alisson L. Selistre on 02/12/17.
+//  Copyright © 2017 Alisson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
